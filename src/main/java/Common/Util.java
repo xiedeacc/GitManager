7abf290855466ee2bcea9155904ceb4554fa602a
xiedeacc <xiedeacc@gmail.com>
2024-09-27 20:22:42 +0800

    update

--- src/main/java/Common/Util.java
+++ src/main/java/Common/Util.java
@@ -357,7 +357,7 @@ public class Util {
                 return false;
             }
 
-            PUSH_ARGS = new String[]{"push", "-u", "origin" ,"--all"};
+            PUSH_ARGS = new String[]{"push", "-u", "origin" ,"--all", "-f"};
             builder = FS.DETECTED.runInShell("git", PUSH_ARGS);
             builder.directory(repo_dir);
             os = new ByteArrayOutputStream();
